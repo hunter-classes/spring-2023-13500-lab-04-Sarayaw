175d744e7c761a5b3f68c531ed2020d5ec9da964
Sarayaw <104982620+Sarayaw@users.noreply.github.com>
2023-02-23 20:08:44 -0500

    Update box.cpp

--- box.cpp
+++ box.cpp
@@ -1,16 +1,61 @@
 #include <iostream>
+#include "funcs.h"
+#include <string>
 
-int main() {
-  int width, height;
-  std::cout << "Enter width and height of the box: \n";
-  std::cin >> width >> height;
+int main ()
+{
+  std::cout << "\n" << "Task A:" << "\n";
+  std::cout << "\n------------------\n";
+  std::cout << box(5,7);
+  std::cout << "\n------------------\n";
+  std::cout << box(18,28);
+  std::cout << "\n------------------\n";
 
-  std::cout << std::endl;
-  std::cout << "Shape: \n";
-  for (int i = 0; i < height; i++) {
-    for (int j = 0; j < width; j++) {
-      std::cout << "*";
-    }
-    std::cout << std::endl;
-  }
+ 
+  std::cout << "\n" << "Task B:" << "\n";
+  std::cout << "\n------------------\n";
+  std::cout << checkerboard(5,1);
+  std::cout << "\n------------------\n";
+  std::cout << checkerboard(14,15);
+  std::cout << "\n------------------\n";
+
+  std::cout << "\n" << "Task C:" << "\n";
+  std::cout << "\n------------------\n";
+  std::cout << cross(10);
+  std::cout << "\n------------------\n";
+  std::cout << cross(15);
+  std::cout << "\n------------------\n";
+  
+
+  std::cout << "\n" << "Task D" << "\n";
+  std::cout << "\n------------------\n";
+  std::cout << lower(8);
+  std::cout << "\n------------------\n";
+  std::cout << lower(11);
+  std::cout << "\n------------------\n";
+
+  std::cout << "\n" << "Task E:" << "\n";
+  std::cout << "\n------------------\n";
+  std::cout << upper(4);
+  std::cout << "\n------------------\n";
+  std::cout << upper(5);
+  std::cout << "\n------------------\n";
+
+  
+ 
+  std::cout << "\n" << "Task F:" << "\n";
+  std::cout << "\n------------------\n";
+  std::cout << trapezoid(12,5);
+  std::cout << "\n------------------\n";
+  std::cout << trapezoid(13,9);
+  std::cout << "\n------------------\n";
+
+  
+  std::cout << "\n" << "Task G:" << "\n";
+  std::cout << "\n------------------\n";
+  std::cout << checkerboard3x3(24,24);
+  std::cout << "\n------------------\n";
+  std::cout << checkerboard3x3(6,12);
+  std::cout << "\n------------------\n";
+  
 }
